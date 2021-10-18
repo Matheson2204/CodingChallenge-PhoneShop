b70a036e6649647c5d945fa919bf1f96ad0a61d7
Matheson_Walsh <mathesonw@optusnet.com.au>
2021-10-18 13:22:22 +1100

    completed working functionality to delete and quantity

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # CodingChallenge-PhoneShop
+
+Started out designing the HTML elements of the webpage to construct the item cards, then the order detail grid. Used the BootStrap elements for the card-body, and the grid.
+
+Next up I started styling the HTML with CSS for the cards and the grid. theres a few descrepencies with the design, that i'm still working on the fix but I desided to leave that as a small priority to work on the JavaScript functionality of the webpage.
+
+The Javascript functionality consist of 2 buttons for submit aka 'add to cart'(work in progress) and the delete button aka 'Remove' within the grid. I also added form control input box for the quantity of the target item which disables the input box ability for quantity to be less then 1.
+
+Other functionality includes the ability to updating the grids boxes with new data for the card and add the price and quantity for a total price of the item. Then add all the items totals into a overall Total price (still working on that).
+
+Dev Tools, Languages:
+-Langages:
+- HTML
+- CSS
+- JavaScript
+-Dev Tools:
+- BootStrap 
+
+
+
--- main.js
+++ main.js
@@ -18,19 +18,21 @@ for (let i = 0; i < submitBtn.length; i++) {
     submit.addEventListener("click", addCartItem)
     }
 }
-const quantityInput = docuemnt.getelementById('#quantForm')
+const quantityInput = document.getElementsByClassName('form-control')
 for (let i = 0; i < quantityInput.length; i++) {
     let input =  quantityInput[i]
     input.addEventListener('change', quantityChanged)
+}
 
-function addCartItem(event) {
-    const buttonClicked = event.target
-    const shopItem = buttonClicked.parentElement.parentElement
-    const title = shopItem.getElementsByClassName('card-title')[0]
-    const price = shopItem.getElementsByClassName('card-text')[0]
-    console.log(title, price)
-    
+const removeOrderItemButton = document.getElementsByClassName('delete-btn')
+console.log(removeOrderItemButton)
+for (let i = 0; i < removeOrderItemButton.length; i++) {
+    const deleteButton = removeOrderItemButton[i]
+    deleteButton.addEventListener('click', function(event) { 
+        const buttonClicked = event.target
+     buttonClicked.parentElement.parentElement.remove()
     updateOrderTotal()
+    })
 }
 
 function quantityChanged(event) {
@@ -40,26 +42,87 @@ function quantityChanged(event) {
     }
     updateOrderTotal()
 }
+
+function addCartItem(event) {
+    const addButtonClicked = event.target
+    const card = addButtonClicked.parentElement.parentElement
+    const title = card.getElementsByClassName('card-title')[0].innerText
+    const price = card.getElementsByClassName('card-text')[0].innerText
+    console.log(title, price)
+    addCartItem(title, price)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price) {
+const cartRow = document.createElement('div')
+cartRow = classlist.add('col') 
+cartRow.innerText = title
+const cartItems = document.getElementsByClassName('card')[0]
+const cartItemNames = cartItems.getElementsByClassName('card-title')
+for (let i = 0; i < cartItemNames.length; i++) {
+if (cartItemNames[i].innerText == title) {
+    alert('This item is already added to the cart')
+    return
+ }
+}
+const cartRowContents = 
+`<div class="grid-container">
+<div class="row row-cols-5">
+<div class="col">Item Name</div>
+<div class="col">Quantity </div>
+<div class="col" >Price</div>
+<div class="col">Total</div>
+<div class="col">Action</div>
+</div>
+<div class="row row-cols-5">
+<div class="col">Samsung J2 Pro</div>
+<div class="col">${quantity}</div>
+<div class="col">${price}</div>
+<div class="col">${total}</div>
+<div class="col">
+  <button class="btn delete-btn" type="button">Remove</button>
+</div>
+</div>
+  <div class="row row-cols-5">
+<div class="col">Hp Notebook</div>
+<div class="col col">12</div>
+<div class="col">13</div>
+<div class="col">14</div>
+<div class="col">
+  <button class="btn delete-btn" type="button">Remove</button></div>
+</div>
+<div class="row row-cols-5">
+<div class="col">Panasonic T44 Lite</div>
+<div class="col">17</div>
+<div class="col">18</div>
+<div class="col">19</div>
+<div class="col">
+  <button class="btn delete-btn" type="button">Remove</button></div>
+</div>
+<div class="row row-cols-3">
+<div class="col col-md-7" id="labelPriceTotal">Overall Total</div>
+<div class="col col-md-2.5" id="overallPrice">22</div>
+<div class="col">
+  <button class="btn delete-btn" type="button">Remove</button></div>
+</div>
+</div>`
+  cartRow.innerHTML = cartRowContents
+  cartItems.append(cartRow)
+  cartRow.getElementsByClassName('delete-btn')
+}
+
     function updateOrderTotal() {
-        const cardItem = document.getElementById('#itemId')[0]
-        const itemData = cardItem.getElementsByClassName('row')
-        const total = 0;
-        for (let i = 0; i < itemData.length; i++) {
-            let itemData = itemData[i]
-            const priceElement = document.querySelectorAll('p')[0]
-            const quantityElement = itemData.getElementById('#quantForm')
+       let cardContainer = document.getElementByClassName('card-group')[0]
+        let card = cardContainer.getElementsByClassName('card')
+        for (let i = 0; i < card.length; i++) {
+            let card = card[i]
+            let priceElement = card.getElementsByClassName('card-text')[0]
+            let quantityElement = card.getElementsByClassName('form-control')
             const price = parseFloat(priceElement.innerText.replace('$', ''))
             const quantity = quantityElement.value
             console.log(price)
             total = total + ([price * quantity])
         }
         total = Math.round(total * 100) / 100
-        document.getElementById('#totalPrice')[0].innerText = '$' + total
+        document.getElementById('#overallPrice')[0].innerText = '$' + total
     }
-    
-    
-
-
-
-
-
